2814e85a761630a92f8714b41d892dbd35b542f3
RobAnt13 <116080051+RobAnt13@users.noreply.github.com>
2023-03-05 21:13:22 +0100

    Create cors.js

---
+++ api/models/cors.js
@@ -0,0 +1,16 @@
+const cors = require('cors');
+
+const whitelist = ['https://robant-chat-app.netlify.app'];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+module.exports = cors(corsOptions);
