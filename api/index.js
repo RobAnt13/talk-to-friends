94bb97b42439aea4698ff220d286908249427f8b
RobAnt13 <116080051+RobAnt13@users.noreply.github.com>
2023-03-06 17:04:52 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,11 +24,11 @@ const corsOptions = {
 };
 
 
-app.use(function(req, res, next) {
+/* app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://robant-chat-app.netlify.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+}); */
 
 
 app.use(cors({
