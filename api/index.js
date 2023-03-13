c59a03936035d3686160b9be26d981c876bebe1d
RobAnt13 <116080051+RobAnt13@users.noreply.github.com>
2023-03-13 11:01:36 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,18 +11,7 @@ const Message = require('./models/Message');
 const ws = require('ws');
 const fs = require('fs');
 
-
-
 const backendUrl = 'https://chat-app-f8hj.onrender.com:8080/';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App backendUrl={backendUrl} />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL, (err) => {
@@ -36,51 +25,13 @@ const corsOptions = {
   credentials: true,
 };
 
-
-/* app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://robant-chat-app.netlify.app");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-}); 
-
-
-app.use(cors({
-  credentials: true,
-  origin: 'https://robant-chat-app.netlify.app'
-})); */
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
-
-/* app.get('/profile', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'https://robant-chat-app.netlify.app');
-  // rest of the code to handle the request
-}); */
-
-
 
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(express.json());
 app.use(cookieParser());
 
-/*
-app.use(cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL,
-}));
-
-async function getUserDataFromRequest(req) {
-    return new Promise((resolve, reject) => {
-    const token = req.cookies?.token;
-    if (token) {
-        jwt.verify(token, jwtSecret, {}, (err, userData) => {
-            if (err) throw err;
-            resolve(userData);
-            });
-        } else {
-            reject('no token');
-        }
-    });
-} */
 
 app.get('/test', (req,res) => {
     res.json('test ok');
@@ -115,58 +66,6 @@ app.get('/people', async (req,res) => {
 });
 
 module.exports = app;
-
-/* app.get('/profile', (req,res) => {
-    const token = req.cookies?.token;
-    if (token) {
-        jwt.verify(token, jwtSecret, {}, (err, userData) => {
-            if (err) throw err;
-            res.json(userData);
-        });
-    } else {
-        res.status(401).json('no token');
-    }
-});
-
-app.post('/login', async (req,res) => {
-    const {username, password} = req.body;
-    const foundUser = await User.findOne({username});
-    if (foundUser) {
-        const passOk = bcrypt.compareSync(password, foundUser.password);
-        if (passOk) {
-            jwt.sign({userId:foundUser._id,username}, jwtSecret, {}, (err, token) => {
-                res.cookie('token', token, {sameSite:'none', secure:true}).json({
-                    id: foundUser._id,
-                });
-        });
-        }
-    }
-
-});
-
-app.post('/logout', (req,res) => {
-    res.cookie('token', '', {sameSite:'none', secure:true}).json('ok');
-});
-
-app.post('/register', async (req,res) => {
-    const {username,password} = req.body;
-    try {
-        const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
-        const createdUser = await User.create({                    username:username,  
-        password:hashedPassword 
-    });
-        jwt.sign({userId:createdUser._id,username}, jwtSecret, {}, (err, token) => {
-            if (err) throw err;
-            res.cookie('token', token, {sameSite:'none', secure:true}).status(201).json({
-                id: createdUser._id,
-            });
-        });
-    } catch(err) {
-        if (err) throw err;
-        res.status(500).json('error');
-    }
-}); */
-
 
 
 const server = app.listen(8080);
