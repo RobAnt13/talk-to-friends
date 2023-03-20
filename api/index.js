1fd13e33b4ca0fffb260d4864b644b89d604aa24
RobAnt13 <116080051+RobAnt13@users.noreply.github.com>
2023-03-20 12:19:03 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -23,6 +23,17 @@ const corsOptions = {
   credentials: true,
 };
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 
